71346e9b4fdda070d7de24f102add3dce3c680ad
itzsebas121 <sebastipan121@gmail.com>
2025-04-30 17:05:02 -0500

    App finishim

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,207 @@
+// Flutter code for drawing long division like on paper with step-by-step process including decimals
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(LongDivisionApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class LongDivisionApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'División Larga',
+      home: DivisionInputPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class DivisionInputPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _DivisionInputPageState createState() => _DivisionInputPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _DivisionInputPageState extends State<DivisionInputPage> {
+  final TextEditingController _dividendoController = TextEditingController();
+  final TextEditingController _divisorController = TextEditingController();
+  String? _dividendo;
+  String? _divisor;
 
-  void _incrementCounter() {
+  void _startDivision() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _dividendo = _dividendoController.text;
+      _divisor = _divisorController.text;
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _dividendoController.clear();
+      _divisorController.clear();
+      _dividendo = null;
+      _divisor = null;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      appBar: AppBar(title: Text('División Larga Manual')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            TextField(
+              controller: _dividendoController,
+              decoration: InputDecoration(labelText: 'Dividendo (A)'),
+              keyboardType: TextInputType.number,
             ),
+            TextField(
+              controller: _divisorController,
+              decoration: InputDecoration(labelText: 'Divisor (B)'),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: _startDivision,
+                  child: Text('Dividir'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: _reset,
+                  child: Text('Limpiar'),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            if (_dividendo != null && _divisor != null)
+              Expanded(
+                child: InteractiveViewer(
+                  child: CustomPaint(
+                    painter: LongDivisionPainter(
+                      dividendo: _dividendo!,
+                      divisor: _divisor!,
+                    ),
+                    size: Size(1500, 1000),
+                  ),
+                ),
+              ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class LongDivisionPainter extends CustomPainter {
+  final String dividendo;
+  final String divisor;
+  final double fontSize = 22;
+  final double padding = 30;
+
+  LongDivisionPainter({required this.dividendo, required this.divisor});
+
+  void drawText(Canvas canvas, String text, double x, double y) {
+    final tp = TextPainter(
+      text: TextSpan(
+        text: text,
+        style: TextStyle(fontSize: fontSize, color: Colors.black),
+      ),
+      textDirection: TextDirection.ltr,
+    );
+    tp.layout();
+    tp.paint(canvas, Offset(x, y));
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paintLine = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 2;
+
+    try {
+      BigInt a = BigInt.parse(dividendo);
+      BigInt b = BigInt.parse(divisor);
+
+      int xOffset = padding.toInt();
+      int yOffset = padding.toInt();
+      int cursorX = xOffset;
+      int stepY = yOffset + 80;
+
+      String cociente = '';
+      String actual = '';
+
+      int i = 0;
+      bool started = false;
+      while (i < dividendo.length || (actual != '0' && cociente.length < 30)) {
+        if (i < dividendo.length) {
+          actual += dividendo[i];
+          i++;
+        } else {
+          if (!cociente.contains('.')) {
+            cociente += '.';
+          }
+          actual += '0';
+        }
+
+        BigInt actualNum = BigInt.parse(actual);
+        if (actualNum >= b) {
+          BigInt div = actualNum ~/ b;
+          BigInt mult = div * b;
+          BigInt newResto = actualNum - mult;
+
+          cociente += div.toString();
+
+          drawText(canvas, actualNum.toString(), cursorX.toDouble(), stepY.toDouble());
+          drawText(canvas, '-' + mult.toString(), cursorX.toDouble(), stepY.toDouble() + fontSize);
+
+          canvas.drawLine(
+            Offset(cursorX.toDouble(), stepY.toDouble() + fontSize * 2),
+            Offset(cursorX.toDouble() + mult.toString().length * fontSize * 0.6 + 10, stepY.toDouble() + fontSize * 2),
+            paintLine,
+          );
+
+          stepY += 3 * fontSize.toInt();
+          actual = newResto.toString();
+          if (actual == '0') actual = '';
+
+          cursorX += (div.toString().length * fontSize * 0.6).toInt() + 10;
+          started = true;
+        } else {
+          if (started) {
+            cociente += '0';
+            cursorX += (fontSize * 0.6).toInt();
+          }
+        }
+      }
+
+      double divisionLineX = xOffset + dividendo.length * fontSize * 0.6 + 30;
+
+      // Draw top layout: dividend | divisor
+      drawText(canvas, dividendo, xOffset.toDouble(), yOffset.toDouble());
+      drawText(canvas, '', divisionLineX, yOffset.toDouble());
+      drawText(canvas, divisor, divisionLineX + fontSize, yOffset.toDouble());
+
+      // Vertical line of division
+      canvas.drawLine(
+        Offset(divisionLineX, yOffset.toDouble()),
+        Offset(divisionLineX, yOffset + fontSize + 10),
+        paintLine,
+      );
+
+      // Horizontal line under divisor and result
+      canvas.drawLine(
+        Offset(divisionLineX, yOffset + fontSize + 10),
+        Offset(divisionLineX + cociente.length * fontSize * 0.6 + fontSize, yOffset + fontSize + 10),
+        paintLine,
+      );
+
+      // Result above the division bar
+      drawText(canvas, cociente, divisionLineX + fontSize, yOffset - fontSize - 10);
+    } catch (e) {
+      drawText(canvas, 'Error: Entrada inválida', padding, padding);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
